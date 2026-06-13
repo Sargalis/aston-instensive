bf35cec4fa08ae7b3d9215759be71763d53a4242
Sargalis <lislenov@yandex.ru>
2026-06-13 21:19:02 +0300

    task2

---
+++ task2/src/main/java/org/example/UserDAO.java
@@ -0,0 +1,75 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.List;
+
+public class UserDAO {
+	private static final Logger log = LoggerFactory.getLogger(UserDAO.class);
+
+	// CREATE
+	public User create(User user) {
+		Transaction tx = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			tx = session.beginTransaction();
+			session.persist(user);
+			tx.commit();
+			log.info("Создан пользователь: {}", user.getEmail());
+			return user;
+		} catch (Exception e) {
+			if (tx != null) tx.rollback();
+			log.error("Ошибка создания", e);
+			throw new RuntimeException("Не удалось создать пользователя", e);
+		}
+	}
+
+	// READ (по id)
+	public User read(Long id) {
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			return session.get(User.class, id);
+		}
+	}
+
+	// READ (все)
+	public List<User> readAll() {
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			return session.createQuery("FROM User", User.class).list();
+		}
+	}
+
+	// UPDATE
+	public User update(User user) {
+		Transaction tx = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			tx = session.beginTransaction();
+			User merged = session.merge(user);
+			tx.commit();
+			log.info("Обновлен пользователь: {}", user.getEmail());
+			return merged;
+		} catch (Exception e) {
+			if (tx != null) tx.rollback();
+			log.error("Ошибка обновления", e);
+			throw new RuntimeException("Не удалось обновить пользователя", e);
+		}
+	}
+
+	// DELETE
+	public void delete(Long id) {
+		Transaction tx = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			tx = session.beginTransaction();
+			User user = session.get(User.class, id);
+			if (user != null) {
+				session.remove(user);
+				log.info("Удален пользователь с ID: {}", id);
+			}
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null) tx.rollback();
+			log.error("Ошибка удаления", e);
+			throw new RuntimeException("Не удалось удалить пользователя", e);
+		}
+	}
+}
